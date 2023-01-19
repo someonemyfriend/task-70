200847a80b884e7b8f724e6d58e1dd6c631e8997
Vasilia Staykova <someonemyfriend@gmail.com>
2023-01-19 13:13:23 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,27 @@
-import "../scss/app.scss";
+//import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let tryToGetElement = document.getElementsByClassName("main");
+  console.log(tryToGetElement);
+  
+  let tryToCreateDiv = document.createElement('div');
+  tryToCreateDiv.classList.add('image');
+  console.log(tryToCreateDiv);
+  
+  let tryToCreateImage = document.createElement("img");
+  tryToCreateImage.src = "https://picsum.photos/200/301";
+  tryToCreateDiv.appendChild(tryToCreateImage);
+
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
 });
+
+
+
+
+
