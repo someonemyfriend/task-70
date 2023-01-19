834b8777e485ff4d6e9dadce36bd3007404ec582
Vasilia Staykova <someonemyfriend@gmail.com>
2023-01-19 15:37:04 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,6 @@ window.addEventListener("DOMContentLoaded", () => {
 let imageScale = document.querySelector(".image").addEventListener("click", scaleImage);;
 
 function scaleImage() {
-  document.getElementById("myImage").style.width = "150%";
-  document.getElementById("myImage").style.height = "150%";
+  document.getElementById("myImage").style.transform = "scale(2)";
 }
 
